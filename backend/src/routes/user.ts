6428c98f69cde4434a91e429f8df183d8f430903
Darshan98Solanki <darshan98solanki@gmail.com>
2025-01-01 20:56:20 +0530

    frontend update

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -2,7 +2,7 @@ import { Hono } from 'hono'
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
 import { sign, verify } from 'hono/jwt'
-import { loginSchema, updateUserSchema, UpdateUserSchema, userSchema } from '@darshan98solanki/medium-common'
+import { loginSchema, updateUserSchema, userSchema } from '@darshan98solanki/medium-common'
 import crypto from "crypto-js"
 
 export const UserRouter = new Hono<{
